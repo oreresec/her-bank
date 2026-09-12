d94ec39db86928f0eecdb57ed83471994fa709cf
vrimsec <abayomiadeboye@aol.com>
2026-09-12 07:40:06 -0400

    rate limiting & responsive

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 const globalLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 1000,
+    max: 20,
     message:{error: "Too many requests, please try again later"},
     standardHeaders: true,
     legacyHeaders: false
